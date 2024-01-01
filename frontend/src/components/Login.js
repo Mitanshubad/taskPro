26b96d48adbddea0842891d4ee1f5be6cce91d7a
Mitanshubad <119037058+Mitanshubad@users.noreply.github.com>
2024-01-01 18:30:47 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -11,7 +11,7 @@ function Login(props) {
   const handelSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://taskprodeploy.onrender.com/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
